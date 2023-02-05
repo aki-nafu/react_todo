3a322895184e9dc1c096dfd19f192bdfc7d0a924
aki-nafu <akimifunakoshi@gmail.com>
2023-02-05 18:30:25 +0900

    「追加」ボタンを追加

--- todo/src/components/InputToDo.jsx
+++ todo/src/components/InputToDo.jsx
@@ -11,13 +11,11 @@ export const InputToDo = (props) => {
 
     // Enter押下時、ToDoに追加
     const handleEnter = e => {
-        if (e.key === 'Enter') {
-            // 入力値が空白文字の場合終了
-            if (!text.match(/\S/g)) return;
-            // ToDoAppクラスの「handleAdd」関数を実行
-            props.onAdd(text);
-            setText('');
-        }
+        // 入力値が空白文字の場合終了
+        if (!text.match(/\S/g)) return;
+        // ToDoAppクラスの「handleAdd」関数を実行
+        props.onAdd(text);
+        setText('');
     };
 
     return (
@@ -28,8 +26,8 @@ export const InputToDo = (props) => {
                 placeholder="Enter to add"
                 value={text}
                 onChange={handleChange}
-                onKeyPress={handleEnter}
             />
+            <button onClick={handleEnter} class="button">追加</button>
         </div>
     );
 }
